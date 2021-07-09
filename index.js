ce32bc736c8590d57db987f885a1e422371e852d
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-09 23:09:38 +1000

    last compiling

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const computer_vision_1 = require("./computer-vision");
+class SteeringControl {
+    execute(command) {
+        console.log(`Executing: ${command}`);
+    }
+    turn(direction) {
+        this.execute(`Turn ${direction}`);
+    }
+}
 class Car {
     constructor(props) {
         this.isRunning = props.isRunning;
+        this.steeringControl = props.steeringControl;
+    }
+    respond(events) {
+        if (!this.isRunning) {
+            return console.log('The car is off');
+        }
+        Object.keys(events).forEach(eventKey => {
+            if (!events[eventKey]) {
+                return;
+            }
+            if (eventKey === 'obstacleLeft') {
+                this.steeringControl.turn('right');
+            }
+            if (eventKey === 'obstacleRight') {
+                this.steeringControl.turn('left');
+            }
+        });
     }
 }
-const autonomousCar = new Car({ isRunning: true });
-console.log(autonomousCar.isRunning);
+const steering = new SteeringControl();
+const autonomousCar = new Car({ isRunning: true, steeringControl: steering });
+autonomousCar.respond(computer_vision_1.getObstacleEvents());
