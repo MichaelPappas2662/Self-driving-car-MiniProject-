fe7d1fd9900067da207caddef58c25a7b8dd4d53
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-09 23:09:15 +1000

    finishing the code

--- index.ts
+++ index.ts
@@ -7,28 +7,63 @@ interface AutonomousCar {
 
 interface AutonomousCarProps {
     isRunning?: boolean;
+    steeringControl: Steering;
 }
 
 interface Events {
     [event:string]:boolean;
 }
 
+interface Control {
+    execute: (command: string) => void;
+}
 
+interface Steering extends Control {
+    turn: (direction: string) => void;
+}
+
+class SteeringControl implements Steering {
+    execute(command: string){
+        console.log(`Executing: ${command}`);
+    }
+
+    turn(direction: string) {
+        this.execute(`Turn ${direction}`)
+    }
+}
 
 class Car implements AutonomousCar{
     isRunning;
+    steeringControl;
 
     constructor(props: AutonomousCarProps) {
         this.isRunning = props.isRunning;
+        this.steeringControl = props.steeringControl;
     }
 
     respond(events: Events) {
         if(!this.isRunning){
             return console.log('The car is off');
         }
+
+        Object.keys(events).forEach(eventKey => {
+           if (!events[eventKey]){
+               return;
+           }
+           
+           if (eventKey === 'obstacleLeft'){
+               this.steeringControl.turn('right');
+           }
+
+           if (eventKey === 'obstacleRight'){
+            this.steeringControl.turn('left');
+        }
+        })
     }
 }
 
-const autonomousCar = new Car({isRunning: true});
+const steering = new SteeringControl();
+const autonomousCar = new Car({isRunning: true, steeringControl:steering});
+
 
 autonomousCar.respond(getObstacleEvents());
